5dac0d3c57e8ea2765a5ade26ac7acd6ec9fa9c4
Tyrxn <tyran.wt00@gmail.com>
2023-06-25 22:40:26 +1000

    added search functionality

--- src/App.js
+++ src/App.js
@@ -3,20 +3,40 @@ import CharacterList from './components/CharacterList';
 import Team from './components/Team';
 
 const App = () => {
-  const [team, setTeam] = useState([]);
+const [team, setTeam] = useState([]);
+const [selectedTeamBox, setSelectedTeamBox] = useState(null);
 
-  const handleAddToTeam = (character, position) => {
-    const player = { character, position };
-    setTeam([...team, player]);
-  };
+const handleAddToTeam = (character, position) => {
+if (selectedTeamBox !== null) {
+const updatedTeam = [...team];
+updatedTeam[selectedTeamBox] = { character, position };
+setTeam(updatedTeam);
+setSelectedTeamBox(null);
+}
+};
 
-  return (
-    <div>
-      <h1>Marvel Team Selector</h1>
-      <CharacterList onAddToTeam={handleAddToTeam} />
-      <Team team={team} />
-    </div>
-  );
+const handleSelectTeamBox = (index) => {
+setSelectedTeamBox(index);
+};
+
+const handleDeletePlayer = (index) => {
+const updatedTeam = [...team];
+updatedTeam[index] = null;
+setTeam(updatedTeam);
+};
+
+return (
+<div>
+<h1>Marvel Team Selector</h1>
+<Team
+     team={team}
+     selectedTeamBox={selectedTeamBox}
+     onSelectTeamBox={handleSelectTeamBox}
+     onDeletePlayer={handleDeletePlayer}
+   />
+<CharacterList onAddToTeam={handleAddToTeam} />
+</div>
+);
 };
 
 export default App;
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,44 +1,59 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Character from './Character';
 import md5 from 'md5';
 
 const CharacterList = ({ onAddToTeam }) => {
   const [characters, setCharacters] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const publicKey = 'e76a3db942f6ef46df3dd05e04b25d16';
+  const privateKey = '01aee429844718163e717a6ddf16368bd5f17f4b';
 
-  useEffect(() => {
-    const publicKey = 'e76a3db942f6ef46df3dd05e04b25d16';
-    const privateKey = '01aee429844718163e717a6ddf16368bd5f17f4b';
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+    searchCharacters(event.target.value);
+  };
+
+  const searchCharacters = async (query) => {
     const ts = new Date().getTime().toString();
     const hash = md5(ts + privateKey + publicKey);
 
-    const fetchCharacters = async () => {
+    try {
       const response = await axios.get(
-        `https://gateway.marvel.com/v1/public/characters?apikey=${publicKey}&ts=${ts}&hash=${hash}`
+        `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${query}&apikey=${publicKey}&ts=${ts}&hash=${hash}`
       );
 
       setCharacters(response.data.data.results);
-    };
-
-    fetchCharacters();
-  }, []);
+    } catch (error) {
+      console.log('Error searching characters:', error);
+    }
+  };
 
   const containerStyle = {
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'left',
-
   };
 
   return (
-    <div style={containerStyle}>
-      {characters.map((character) => (
-        <Character
-          key={character.id}
-          character={character}
-          selectForTeam={onAddToTeam}
+    <div>
+      <div style={{ marginBottom: '10px' }}>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          placeholder="Search character..."
         />
-      ))}
+      </div>
+      <div style={containerStyle}>
+        {characters.map((character) => (
+          <Character
+            key={character.id}
+            character={character}
+            selectForTeam={onAddToTeam}
+          />
+        ))}
+      </div>
     </div>
   );
 };
