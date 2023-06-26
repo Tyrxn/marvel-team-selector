a8bf745c734377fbdaba0a8d702cb00ffbecbfb6
Tyrxn <tyran.wt00@gmail.com>
2023-06-26 20:13:13 +1000

    files

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
 import React, { useState } from 'react';
-import CharacterList from './components/CharacterList';
-import Team from './components/Team';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CharacterList from './components/characters/CharacterList';
+import Team from './components/characters/Team';
+import WelcomeScreen from './components/WelcomeScreen';
+import SelectionScreen from './components/SelectionScreen';
+import ResultsScreen from './components/ResultsScreen';
 
 const App = () => {
-const [team, setTeam] = useState([]);
-const [selectedTeamBox, setSelectedTeamBox] = useState(null);
+  const [team, setTeam] = useState([]);
+  const [selectedTeamBox, setSelectedTeamBox] = useState(null);
 
-const handleAddToTeam = (character, position) => {
-if (selectedTeamBox !== null) {
-const updatedTeam = [...team];
-updatedTeam[selectedTeamBox] = { character, position };
-setTeam(updatedTeam);
-setSelectedTeamBox(null);
-}
-};
+  const handleAddToTeam = (character, position) => {
+    if (selectedTeamBox !== null) {
+      const updatedTeam = [...team];
+      updatedTeam[selectedTeamBox] = { character, position };
+      setTeam(updatedTeam);
+      setSelectedTeamBox(null);
+    }
+  };
 
-const handleSelectTeamBox = (index) => {
-setSelectedTeamBox(index);
-};
+  const handleSelectTeamBox = (index) => {
+    setSelectedTeamBox(index);
+  };
 
-const handleDeletePlayer = (index) => {
-const updatedTeam = [...team];
-updatedTeam[index] = null;
-setTeam(updatedTeam);
-};
+  const handleDeletePlayer = (index) => {
+    const updatedTeam = [...team];
+    updatedTeam[index] = null;
+    setTeam(updatedTeam);
+  };
 
-return (
-<div>
-<h1>Marvel Team Selector</h1>
-<Team
-     team={team}
-     selectedTeamBox={selectedTeamBox}
-     onSelectTeamBox={handleSelectTeamBox}
-     onDeletePlayer={handleDeletePlayer}
-   />
-<CharacterList onAddToTeam={handleAddToTeam} />
-</div>
-);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomeScreen />} />
+        <Route path="/selection" element={<SelectionScreen />} />
+        <Route path="/results" element={<ResultsScreen />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
